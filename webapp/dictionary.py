f15e16e6decad8789fe9019de28fa4536f927dba
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-08-26 13:05:25 +0400

    .

--- webapp/dictionary.py
+++ webapp/dictionary.py
@@ -1,4 +1,5 @@
 import justpy as jp
+import definition
 
 
 class Dictionary:
@@ -12,18 +13,17 @@ class Dictionary:
         jp.Div(a=div, text="Get the definition of any English word that you type", classes="text-lg")
 
         input_div = jp.Div(a=div, classes="grid grid-cols-2")
-        jp.Input(a=input_div, placeholder="Type a word in here...",
-                 classes="m-2 bg-gray-100 border-2 border-gray-200 rounded w-64 focus:outline-none "
-                         "focus:border-purple-500 py-2 px-4 focus:bg-white")
 
         output_div = jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40")
+        input_box = jp.Input(a=input_div, placeholder="Type a word in here...", outputdiv=output_div,
+                             classes="m-2 bg-gray-100 border-2 border-gray-200 rounded w-64 focus:outline-none "
+                                     "focus:border-purple-500 py-2 px-4 focus:bg-white")
 
-        jp.Button(a=input_div, text="Get definition", classes="border-2 text-gray-500", click=cls.get_definition,
-                  ou=output_div)
+        input_box.on('input', cls.get_definition)
 
         return wp
 
     @staticmethod
     def get_definition(widget, msg):
-        widget.ou.text = "IO"
-        print(msg)
+        defined = definition.Definition(widget.value).get()
+        widget.outputdiv.text = " ".join(defined)
