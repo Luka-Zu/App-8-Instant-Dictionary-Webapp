7f8ce6f05b3a3a1a17f11f0a1ef49266189e7622
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-08-27 18:27:03 +0400

    .

--- webapp/home.py
+++ webapp/home.py
@@ -2,11 +2,31 @@ import justpy as jp
 
 
 class Home:
-    path = '/home'
+    path = '/'
 
-    def serve(self):
+    @classmethod
+    def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
-        div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+
+        layout = jp.QLayout(a=wp, view="hHh lpR fFf")
+        header = jp.QHeader(a=layout)
+
+        toolbar = jp.QToolbar(a=header)
+
+
+
+
+        drawer = jp.QDrawer(a=layout, show_if_above=True, v_model="left",
+                            bordered=True)
+        jp.QBtn(a=toolbar, dense=True, flat=True, round=True, icon="menu",
+                click=cls.move_drawer, drawer=drawer)
+
+        jp.QToolbarTitle(a=toolbar, text="Instant dictionary")
+
+
+        container = jp.QPageContainer(a=layout)
+
+        div = jp.Div(a=container, classes="bg-gray-200 h-screen p-2")
         jp.Div(a=div, text="This is an home page", classes="text-4xl m-2")
         jp.Div(a=div, text="""Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -16,4 +36,11 @@ class Home:
                 """, classes="text-lg")
 
         return wp
+
+    @staticmethod
+    def move_drawer(widget, msg):
+        if widget.drawer.value:
+            widget.drawer.value = False
+        else:
+            widget.drawer.value = True
 
